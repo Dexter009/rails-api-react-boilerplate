c28654a27603b23beb61b3a035310d7e1613e025
Armen Avanesi <armenavanesi@live.ca>
2017-06-02 19:59:54 -0400

    Add controller for handling 404/500 errors

---
+++ app/controllers/api/v1/base_controller.rb
@@ -0,0 +1,12 @@
+class Api::V1::BaseController < ApplicationController
+
+# respond_to :json
+
+# private
+
+  # -- improve and use later to format errors better
+  # def render_error(message, status)
+  #   render json: {error: message}, status: status
+  # end
+
+end
