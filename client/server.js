0ca0c7315a44c8daaddf1cf5ab61f6b27bdd9ab8
Armen Avanesi <armenavanesi@live.ca>
2017-06-02 20:52:11 -0400

    Change webpack config file

--- client/server.js
+++ client/server.js
@@ -6,8 +6,7 @@ new WebpackDevServer(webpack(config), {
     publicPath: config.output.publicPath,
     watchOptions: {
       aggregateTimeout: 300,
-      poll: 1000,
-      ignored: /node_modules/
+      poll: 1000
     }
   })
   .listen(3001, '0.0.0.0', function (err, result) {
