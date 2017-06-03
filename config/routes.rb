4a5b521e0e0e57a34ea9c5fee6aababed29fd55a
Armen Avanesi <armenavanesi@live.ca>
2017-06-02 20:36:19 -0400

    Add boilerplate routes for versionned API

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
+# require 'api_constraints'
+# add: `scope module: :v1, constraints: ApiConstraints.new(version: 1) do` to use versioning
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api, defaults: { format: :json } do
+    scope module: :v1 do
+      # add necessary resources here e.g:
+      # resources :sessions, only: [:create, :destroy]
+    end
+  end
+
+  get '/404', to: 'errors#not_found'
+  get '/500', to: 'errors#internal_server_error'
 end
