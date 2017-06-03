e239ed62da7f6ca65ede4f6bf09d37958bd0b159
Armen Avanesi <armenavanesi@live.ca>
2017-06-02 20:31:10 -0400

    Update cors configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,7 +10,8 @@ Rails.application.configure do
   config.eager_load = false
 
   # Show full error reports.
-  config.consider_all_requests_local = true
+  # -- disbled to test 404/500 errors
+  config.consider_all_requests_local = false
 
   # Enable/disable caching. By default caching is disabled.
   if Rails.root.join('tmp/caching-dev.txt').exist?
@@ -28,7 +29,7 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
